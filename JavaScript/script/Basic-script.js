a41f91ed551ddabafd15b2ea35491283115ff993
Saswata <saswatagl123@gmail.com>
2026-02-16 12:52:07 +0530

    JS

---
+++ JavaScript/script/Basic-script.js
@@ -0,0 +1,37 @@
+//  var a = prompt("Enter ypu corrrect Age");
+//     if (a >= 18) {
+//         alert("You are eligible for voting");
+//     }
+//     else {
+//         alert("You are not eligible for voting");
+//     }
+
+
+var users =["bvuruvuruv","rubbrvurvb","rubvrubvrubv","rubvrubvrubvefefef"];
+// for each loop
+users.forEach(function(value){
+    console.log(value);
+});
+
+
+users.forEach(function(value){
+})
+
+var obj1 = {
+    name:"sachin",
+    age: 30,
+    city: "pune",
+    isMarried: false
+};
+
+var obj2 = new Object();
+obj2={
+    name:"Rohit",
+    age: 33,
+    city: "Kolkata",
+    isMarried: true
+}
+
+var obj3 =[obj1,obj2];
+
+console.log(obj3);
