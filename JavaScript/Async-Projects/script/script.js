ec53a719c063ac9ea8e6b0036b5014b6008e91cb
Saswata <saswatagl123@gmail.com>
2026-04-01 17:03:16 +0530

    Color Picker:2

--- JavaScript/Async-Projects/script/script.js
+++ JavaScript/Async-Projects/script/script.js
@@ -2,13 +2,17 @@ function getRandomColor() {
   return '#' + Math.floor(Math.random() * 16777215).toString(16);
 }
 
-document.getElementById("start").addEventListener("click",()=>{
-    const intervalId = setInterval(() => {
-        document.body.style.backgroundColor = getRandomColor();
-    }, 1000);
 
-    document.getElementById("stop").addEventListener("click",()=>{
+let intervalId;
+document.getElementById("start").addEventListener("click",()=>{
+      intervalId = setInterval(() => {
+        document.body.style.backgroundColor = getRandomColor();
+        document.querySelector("h1").style.color = getRandomColor();
+        document.querySelector("h2").style.color = getRandomColor();
+    }, 1000);
+})
+
+ document.getElementById("stop").addEventListener("click",()=>{
         clearInterval(intervalId);
         console.log("Interval Stopped!");
     })
-})
