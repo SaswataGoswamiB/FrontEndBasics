6f6e02fdb8e4489d0227192ab3f7375e3ae56a0c
Saswata <saswatagl123@gmail.com>
2026-04-07 13:10:49 +0530

    fetch

--- JavaScript/script/Async-JS/fetch-script.js
+++ JavaScript/script/Async-JS/fetch-script.js
@@ -2,19 +2,23 @@
 // fetch is async by default.
 // So it oges into call back queue
 //
-console.log("Using Fetch");
-const hey =fetch("https://randomuser.me/api/").
-then(blob=>blob.json())
-.then(read=>console.log(read));
+// console.log("Using Fetch");
+// const hey = fetch("https://randomuser.me/api/").
+// then(blob=>blob.json())
+// .then(read=>console.log(read));
 
-console.log("Printing value of Hey ")
-console.log(hey);
-
-
-
-const data = fetch("https://randomuser.me/api/").
-then(blob=>blob.json()).
-then(data=>console.log(data)).catch(e=>console.log(e))
-.finally(()=>console.log("This will run regardless of the outcome of the fetch operation."));
+// console.log("Printing value of Hey ")
+// console.log(hey);
 
 
+
+// const data = fetch("https://randomuser.me/api/").
+// then(blob=>blob.json()).
+// then(data=>console.log(data)).catch(e=>console.log(e))
+// .finally(()=>console.log("This will run regardless of the outcome of the fetch operation."));
+
+
+const datafinal = await fetch("https://randomuser.me/api/");
+try{
+    console.log("Data is :", await datafinal.json());}
+catch(e){console.log("Error occurred:", e);}
