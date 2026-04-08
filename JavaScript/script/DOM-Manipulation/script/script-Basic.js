15a5a019e8c23d045785ac96b6c5440c824fcd3e
Saswata <saswatagl123@gmail.com>
2026-04-08 19:07:51 +0530

    DOM Manipulation-3

---
+++ JavaScript/script/DOM-Manipulation/script/script-Basic.js
@@ -0,0 +1,13 @@
+
+
+//inner HTML
+document.getElementById("btn").addEventListener("click",()=>{
+    document.getElementById("btn").innerHTML = "<i>Staring.....</i>"
+});
+
+
+//text content
+
+// document.getElementById("btn").addEventListener("click",()=>{
+//     document.getElementById("btn").textContent = "Staring....."
+// });
--- JavaScript/script/DOM-Manipulation/script/script.js
+++ JavaScript/script/DOM-Manipulation/script/script.js
@@ -1,13 +1,11 @@
-
-
-//inner HTML
-document.getElementById("btn").addEventListener("click",()=>{
-    document.getElementById("btn").innerHTML = "<i>Staring.....</i>"
+var statuse = document.getElementById("btn");
+var text = document.getElementById("text");
+statuse.addEventListener("click",()=>{
+    statuse.style.color = "red";
+    statuse.style.fontSize = "30px";
+    text.style.color = "blue";
+    text.style.fontSize = "20px";
+    text.style.fontStyle = "italic";
+    text.style.fontWeight = "bold";
+    text.style.transition = "all 0.5s ease-in-out";
 });
-
-
-//text content
-
-// document.getElementById("btn").addEventListener("click",()=>{
-//     document.getElementById("btn").textContent = "Staring....."
-// });
