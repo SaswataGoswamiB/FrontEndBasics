e249d11697b66b9a692996fce5c99e70b5fdae4c
Saswata <saswatagl123@gmail.com>
2026-04-08 18:47:14 +0530

    DOM Manipulation-2

--- JavaScript/script/DOM-Manipulation/script/script.js
+++ JavaScript/script/DOM-Manipulation/script/script.js
@@ -1,3 +1,13 @@
+
+
+//inner HTML
 document.getElementById("btn").addEventListener("click",()=>{
-    document.getElementById("btn").innerHTML = "Staring....."
+    document.getElementById("btn").innerHTML = "<i>Staring.....</i>"
 });
+
+
+//text content
+
+// document.getElementById("btn").addEventListener("click",()=>{
+//     document.getElementById("btn").textContent = "Staring....."
+// });
