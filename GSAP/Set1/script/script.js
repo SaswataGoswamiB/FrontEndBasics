0a95c1c09ceec6d0f73d74ecd4ae207154ae054a
SASWATA GOSWAMI <85552395+SaswataGoswamiB@users.noreply.github.com>
2026-05-14 22:24:34 +0530

    GSAP-Intil Commit

---
+++ GSAP/Set1/script/script.js
@@ -0,0 +1,42 @@
+// gsap.to("#box",{
+//     x:1000,
+//     rotate : 360,
+//     backgroundColor : "blue",
+//     durataion:2,
+//     delay : 1,
+// });
+
+// gsap.from("#box",{
+//     x:1000,
+//     rotate : 360,
+//     backgroundColor : "blue",
+//     durataion:2,
+//     delay : 1,
+
+// })
+
+//GSAP Time-line
+
+var gsapa = gsap.timeline();
+
+gsap.to("#box1",{
+    x:1200,
+    rotate:360,
+    scale: 0.6,
+    durataion:2,
+    delay:1
+})
+gsapa.to("#box2",{
+    x:1200,
+    rotate:360,
+    scale: 0.6,
+    durataion:2,
+})
+gsapa.to("#box3",{
+    x:1200,
+    rotate:360,
+    scale: 0.6,
+    durataion:2,
+})
+
+//GSAP TIME-LINE
---
+++ GSAP/Set2/script/script.js
@@ -0,0 +1,7 @@
+gsap.from("#nav h3",{
+y:-50,
+delay : 0.4,
+duration : 0.7,
+opacity:0,
+stagger : 0.2
+});
