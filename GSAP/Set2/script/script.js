32c9c29ddc067ff92655ca4611bbc245051d9b19
Saswata <saswatagl123@gmail.com>
2026-05-15 11:01:10 +0530

    Beni.afterwork Landing Page

--- GSAP/Set2/script/script.js
+++ GSAP/Set2/script/script.js
@@ -1,7 +1,21 @@
-gsap.from("#nav h3",{
+var timeline = gsap.timeline();
+
+timeline.from("#image-intro img",{
+    x:100,
+    opacity:0,
+    delay : 0.4,
+});
+
+timeline.from("#nav h3",{
 y:-50,
-delay : 0.4,
-duration : 0.7,
+duration : 0.5,
 opacity:0,
 stagger : 0.2
+});
+
+timeline.from("#main h1 span",{
+    x : -500,
+    duration : 0.6,
+    opacity : 0,
+    stagger : 0.4,
 });
