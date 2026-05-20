03c860b65741e2eabcef7c982691744c864149e8
SASWATA GOSWAMI <85552395+SaswataGoswamiB@users.noreply.github.com>
2026-05-20 17:32:23 +0530

    Locomotive.JS

---
+++ Locomotive/Set-1/script/script.js
@@ -0,0 +1,25 @@
+new LocomotiveScroll({
+    el:document.querySelector("#main"),
+    smooth : true
+});
+
+// gsap.from("#page1 .box",{
+//     scale : 0,
+//     duration : 1,
+//     delay:1,
+//     opacity : 0,
+//     rotate : 360
+// })
+
+// gsap.from("#page3 .box",{
+//     scale : 0,
+//     duration : 1,
+//     opacity : 0,
+//     rotate : 360,
+//     scrollTrigger :{
+//         trigger : "#page3 .box",
+//         scroller : "body",
+//         start : "top 60%",
+//         // markers : true
+//     }
+// })
